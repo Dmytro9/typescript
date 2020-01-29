31f0cec9fc11c471826f94f4146ede355c7bb6a8
Dmytro9 <dpoliuliuk@gmail.com>
2020-01-29 22:44:27 +0200

    Advanced

--- Essential/6_guards.ts
+++ Essential/6_guards.ts
@@ -1,5 +1,14 @@
 type AlertType = 'success' | 'danger' | 'warning';
 
+type Wer = 'a/a' | 'b/b';
+
+function setAlert1(type: Wer) {
+  // ...
+  console.log(type);
+}
+
+setAlert1('a/a');
+
 function setAlert(type: AlertType) {
   // ...
   console.log(type);
